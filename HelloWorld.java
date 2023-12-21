e23b2c649dfe8f95b1cfe09766e101505a31cf00
Elias Henriksen <79273139+eliashenriksen@users.noreply.github.com>
2023-12-21 15:07:45 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args){
-    System.out.println("======Hello World Jenkins Training JAVA file CHANGED======");
+    System.out.println("======Jenkins Webhook Added======");
   }
 }
